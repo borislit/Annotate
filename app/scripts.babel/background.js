559f8080d672bca53ad07ca1347fb15d2a51ce50
Boris Litvinsky <borisl@wix.com>
2015-11-26 20:31:43 +0200

    Initial Commit

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -26,7 +26,6 @@ class MessagingService {
       }
     });
   }
-
 }
 
 MessagingService.registerListeners();
--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -3,7 +3,6 @@
 console.log('\'Allo \'Allo! Content script');
 
 jQuery(function ($) {
-  console.log('Done 4');
   $('.mw-body-content').annotator();
 
   chrome.runtime.onMessage.addListener((data) => {
